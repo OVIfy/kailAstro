7af930e6ad7ed47a6694059b6a03db6a4b6a9fd7
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-02-15 19:23:46 +0100

    chealsea x

---
+++ src/data/chelseax.js
@@ -0,0 +1,57 @@
+const pageData = {
+    overview : [
+        {
+            title : 'My Role',
+            content : ['Product Designer', 'UX Researcher', 'UI/UX Designer']
+        },
+        {
+            title : 'Category',
+            content : ['Sound', 'Luxury', 'Travel', 'Creative Design']
+        },
+        {
+            title : 'Mission',
+            content : ['Introduce Virtual Live Streaming to Chelsea FC.', 'Design a unique and stylish VR headset.']
+        },
+        {
+            title : 'Tools',
+            content : ['Figma', 'Python', 'DraftXR', 'Microsoft Excel']
+        },
+        {
+            title : 'Team',
+            content : ['kamil Oseni', 'Michael Fajemilehin', 'Edwin Uzobehin', 'Don Pedro Kizito']
+        },
+        {
+            title: 'Timeline',
+            content: ['June 2022 - Present']
+        }
+    ],
+
+    painpoints : [
+        {
+            url : "/images/CRACKHEAD/group1.svg",
+            title: 'Music Sharing',
+            content: 'I need to be able to share a song I’m listening to with someone else easily.',
+            alt:'Number one'
+        },
+        {
+            url : "/images/CRACKHEAD/group2.svg",
+            title: 'Easy Pause',
+            content: 'I need an easy way to pause and play music while using my headphones.',
+            alt:'Number two'
+        }, 
+        {
+            url : "/images/CRACKHEAD/group3.svg",
+            title: 'Earpad Replacement',
+            content: 'It’s too much work replacing the ear pads of my headphone when they need to be replaced.',
+            alt:'Number three'
+        },
+        {
+            url : "/images/CRACKHEAD/group4.svg",
+            title: 'Battery Level',
+            content: 'My headphone sometimes die at odd times or when i’m enjoying a song.',
+            alt:'Number four'
+        },
+    ]
+}
+
+export default pageData
