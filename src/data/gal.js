d784dc95ae850188516ff13dcb86f1be186344e2
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-02-18 23:24:22 +0100

    worked on win chel and car

---
+++ src/data/gal.js
@@ -0,0 +1,30 @@
+const pageData = {
+    overview : [
+        {
+            title : 'My Role',
+            content : ['Product Designer', 'UX Researcher', 'UI/UX Designer', 'VR Designer']
+        },
+        {
+            title : 'Category',
+            content : ['Virtual Reality']
+        },
+        {
+            title : 'Mission',
+            content : ['Design a VR world where users can move freely and do whatever they want.']
+        },
+        {
+            title : 'Tools',
+            content : ['Figma', 'DraftXR']
+        },
+        {
+            title : 'Team',
+            content : ['kamil Oseni']
+        },
+        {
+            title: 'Timeline',
+            content: ['Ongoing']
+        }
+    ]
+}
+
+export default pageData
